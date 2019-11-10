1dbd4c06170f386cf98af0a71820937f336f5721
Modest-as <modestas.petravicius@gmail.com>
2019-11-10 02:17:34 +0200

    update dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,10 @@
 
 helm init --client-only
 
-echo $KUBECONFIG_FILE > kubeconfig.yml
+echo $KUBECONFIG_FILE | base64 -d > ./kubeconfig
 
 helm repo update
 
-export KUBECONFIG=$(pwd)/kubeconfig.yml
-
-helm ls
+export KUBECONFIG=$(pwd)/kubeconfig
 
 helm upgrade "$1" "$2"  --namespace="$3" --set="$4" --values="$5" --repo="$6" --username="$7" --password="$8" --wait --atomic --install
